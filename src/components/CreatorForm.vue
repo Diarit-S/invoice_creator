dbae1115a579e1652b8f8f286d0b1215b67474a8
Diarit-S <diarit-94@hotmail.fr>
2020-09-11 13:52:22 +0200

    continue buefy integration and fields creations

--- src/components/CreatorForm.vue
+++ src/components/CreatorForm.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="pdf-content">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css">
     <!-- <input type="text" name="Numero Devis" id="number" v-model="content.title"> -->
     <b-field class="document-type" label="Type de document">
       <b-select placeholder="Type de document" v-model="content.title">
@@ -12,16 +13,48 @@
       </b-select>
       <span class="document-number">N° {{ utilDatas[content.title].number }}</span>
     </b-field>
+    <b-field class="client" label="Client">
+      <b-select placeholder="Client" v-model="content.clientId">
+        <option
+            v-for="(client, index) in clients"
+            :value="index"
+            :key="client.fullName + index">
+            {{ client.fullName }}
+        </option>
+      </b-select>
+      <p> {{ content.clientId >= 0 ? clients[content.clientId].address : '' }}</p>
+    </b-field>
     
+    <b-dropdown aria-role="list" @change="createNewField($event)">
+      <button class="button is-info" slot="trigger">
+          <b-icon icon="plus-circle-outline"></b-icon>
+      </button>
+
+      <b-dropdown-item 
+        v-for="field in possibleFields" 
+        aria-role="listitem" 
+        :key="field"
+        :value="field"
+      >{{ field }}</b-dropdown-item>
+  </b-dropdown>
+
   </div>
 </template>
 
 <script>
 import utilDatas from '@/utils/utilDatas.json'
+import clients from '@/utils/clientDataBase.json'
 
 
 export default {
   name: 'CreatorForm',
+  data() {
+    return {
+      possibleFields: [
+        'text', 'title', 'info'
+      ]
+    }
+  },
   props: {
     content: {
       type: Object,
@@ -31,7 +64,15 @@ export default {
   computed: {
       utilDatas() {
           return utilDatas
+      },
+      clients() {
+        return clients
       }
+  },
+  methods: {
+    createNewField(e) {
+      console.log(e)
+    }
   }
 }
 </script>
@@ -46,6 +87,16 @@ export default {
       .document-number {
         margin-left: 10px;
       }
+    }
+  }
+}
+
+.client {
+  &::v-deep{
+    .field.has-addons {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'buefy/dist/buefy.css'
-import { Input, Field, Select } from 'buefy'
+import { Input, Field, Select, Dropdown, Icon } from 'buefy'
 
 Vue.use(Input)
 Vue.use(Field)
 Vue.use(Select)
+Vue.use(Dropdown)
+Vue.use(Icon)
 
 Vue.config.productionTip = false
 
