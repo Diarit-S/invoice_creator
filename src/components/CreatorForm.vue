b168202cfdfc26e9518794433d942543631d92e2
Diarit-S <diarit-94@hotmail.fr>
2020-09-12 00:31:22 +0200

    start handling field type

--- src/components/CreatorForm.vue
+++ src/components/CreatorForm.vue
@@ -1,42 +1,52 @@
 <template>
   <div class="pdf-content">
     <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css">
-    <!-- <input type="text" name="Numero Devis" id="number" v-model="content.title"> -->
-    <b-field class="document-type" label="Type de document">
-      <b-select placeholder="Type de document" v-model="content.title">
-        <option
-            v-for="type in utilDatas.types"
-            :value="type"
-            :key="type">
-            {{ type }}
-        </option>
-      </b-select>
-      <span class="document-number">N° {{ utilDatas[content.title].number }}</span>
-    </b-field>
-    <b-field class="client" label="Client">
-      <b-select placeholder="Client" v-model="content.clientId">
-        <option
-            v-for="(client, index) in clients"
-            :value="index"
-            :key="client.fullName + index">
-            {{ client.fullName }}
-        </option>
-      </b-select>
-      <p> {{ content.clientId >= 0 ? clients[content.clientId].address : '' }}</p>
-    </b-field>
-    
-    <b-dropdown aria-role="list" @change="createNewField($event)">
-      <button class="button is-info" slot="trigger">
-          <b-icon icon="plus-circle-outline"></b-icon>
-      </button>
+    <div class="left-side">
+      <b-field class="document-type" label="Type de document">
+        <b-select placeholder="Type de document" v-model="content.title">
+          <option
+              v-for="type in utilDatas.types"
+              :value="type"
+              :key="type">
+              {{ type }}
+          </option>
+        </b-select>
+        <span class="document-number">N° {{ utilDatas[content.title].number }}</span>
+      </b-field>
+      <b-field class="client" label="Client">
+        <b-select placeholder="Client" v-model="content.clientId">
+          <option
+              v-for="(client, index) in clients"
+              :value="index"
+              :key="client.fullName + index">
+              {{ client.fullName }}
+          </option>
+        </b-select>
+        <p> {{ content.clientId >= 0 ? clients[content.clientId].address : '' }}</p>
+      </b-field>
+    </div>
 
-      <b-dropdown-item 
-        v-for="field in possibleFields" 
-        aria-role="listitem" 
-        :key="field"
-        :value="field"
-      >{{ field }}</b-dropdown-item>
-  </b-dropdown>
+    <div class="pdf-content__right">
+      <custom-field 
+        v-for="(field, index) in currentFields" 
+        :key="field.type + index" 
+        :field="field"
+      ></custom-field>
+
+      <b-dropdown aria-role="list">
+        <button class="button is-info" slot="trigger">
+            <b-icon icon="plus-circle-outline"></b-icon>
+        </button>
+
+        <b-dropdown-item 
+          v-for="field in possibleFields" 
+          aria-role="listitem" 
+          :key="field"
+          @click="createNewField(field)"
+        >{{ field }}</b-dropdown-item>
+      </b-dropdown>
+    </div>
+    
 
   </div>
 </template>
@@ -45,13 +55,21 @@
 import utilDatas from '@/utils/utilDatas.json'
 import clients from '@/utils/clientDataBase.json'
 
+import CustomField from './CustomField'
+
 
 export default {
   name: 'CreatorForm',
+  components: {
+    CustomField
+  },
   data() {
     return {
       possibleFields: [
         'text', 'title', 'info'
+      ],
+      currentFields: [
+
       ]
     }
   },
@@ -70,14 +88,26 @@ export default {
       }
   },
   methods: {
-    createNewField(e) {
-      console.log(e)
+    createNewField(fieldType) {
+      this.currentFields.push({type: fieldType})
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.pdf-content {
+  display: flex;
+  justify-content: space-around;
+
+  &__right {
+    padding-left: 50px;
+    min-width: 80%;
+  }
+
+}
+
 .document-type {
   &::v-deep{
     .field.has-addons {
---
+++ src/components/CustomField.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <template v-if="field.type === 'text'">
+        <b-field label="Texte">
+            <b-input maxlength="200" type="textarea"></b-input>
+        </b-field>
+      </template>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CustomField',
+  data() {
+    return {
+    }
+  },
+  props: {
+    field: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
